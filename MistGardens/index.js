c618c50938276fb2c69367e3b8b3aa79cfcc697f
nikapailodze <157822500+nikapailodze@users.noreply.github.com>
2024-03-23 00:39:56 +0400

    added nistgarden
    hello

---
+++ MistGardens/index.js
@@ -0,0 +1,16 @@
+// function changeImage(){
+//     const images=[
+//         'url("../images/1.jpg")',
+//         'url("../images/2.avif")',
+//         'url("../images/3.jpg")',
+//         'url("../images/Rectangle\ 4.png")'
+//     ]
+
+//     const bgImage= document.getElementById("bgImage")
+
+//    const bg = images[Math.floor(Math.random()*images.length)]
+
+//    bgImage.style.backgroundImage = bg
+// }
+
+// setInterval(changeImage,1000)
